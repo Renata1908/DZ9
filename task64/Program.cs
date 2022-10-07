57906353d6f93b93030b9923b86c36f55e38f48e
Renata19 <renatalsa@mail.ru>
2022-10-07 20:54:24 +0700

    64

---
+++ task64/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadNumber(string message)
+{
+  Console.WriteLine(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintToN(int n)
+{
+  if (n <= 0)
+  {
+    return;
+  }
+  else
+  {
+    Console.Write(n + " ");
+    PrintToN(n - 1);
+  }
+}
+
+int n = ReadNumber("Введите число N: ");
+PrintToN(n);
